ab1c0b64d4b75af8d557665cb5ab16cdb4dc538d
Warren Wong <warren.wong4@gmail.com>
2021-08-24 18:57:53 -0400

    Setup Bootstrap

--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
+// jquery configuration, possible webpack plugin
+global.jQuery = require('jquery');
+
+// utilizing bootstrap and compiled CSS
+const bootstrap = require('bootstrap');
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(<App />, document.getElementById('app'));
