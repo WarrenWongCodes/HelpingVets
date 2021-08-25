9159044e6c6b5ea8a80a8367b7b6d9db8b8be9c6
Warren Wong <warren.wong4@gmail.com>
2021-08-25 14:10:26 -0400

    Add landing page blurb

---
+++ client/components/Blurb.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// TODO: Add an image, and background image
+export default function Blurb() {
+  return (
+    <section aria-labelledby="blurbSectionHeading">
+      <h1 id="blurbSectionHeading">Helping Veterans Succeed in Tech</h1>
+      <p>
+        A collection of the best resources for transitioning service members and
+        veterans starting a career in tech. From company apprenticeships at
+        Google, and Microsoft to networking/mentorship to help you get to where
+        you want to be.
+      </p>
+      <p>The best part? It's all free and open source!</p>
+    </section>
+  );
+}
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import Blurb from './Blurb.jsx';
 
 // section 1 to 4 goes here
 export default function Home() {
-  return <div></div>;
+  return (
+    <div id="home">
+      <Blurb />
+    </div>
+  );
 }
