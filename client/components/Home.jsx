43360efe83da1448d8bac760cc185a55e016f5fd
Warren Wong <warren.wong4@gmail.com>
2021-08-27 14:15:51 -0400

    Programs Section Setup

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Blurb from './blurb/Blurb.jsx';
-import ProgramsCarousel from './programsCarousel/ProgramsCarousel.jsx';
+import ProgramsSection from './programsSection/ProgramsSection.jsx';
 
 // section 1 to 4 goes here
 export default function Home() {
   return (
     <div id="home">
       <Blurb />
-      <ProgramsCarousel />
+      <ProgramsSection />
     </div>
   );
 }
---
+++ client/components/programsSection/ProgramsSection.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './programsSection.css';
+
+// TODO: add a carousel with cards (different programs)
+// Each card will include:
+// - name, learn more button, quality photo (how to store? AWS S3 bucket), small blurb
+
+export default function ProgramsSection() {
+  return (
+    <section
+      id="programs"
+      className="sectionCenter"
+      aria-labelledby="programSectionHeading"
+    >
+      <h2 id="programSectionHeading">Transition Programs</h2>
+    </section>
+  );
+}
