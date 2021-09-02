edf06942a83e0d1441878e282a6d0be19eaf01a0
Warren Wong <warren.wong4@gmail.com>
2021-09-02 12:56:29 -0400

    Completed Shift Card

--- client/components/programsSection/ProgramCard.jsx
+++ client/components/programsSection/ProgramCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import breakLine from '../../assets/break-line.png';
+import shift from '../../assets/shift.png';
 import './programCard.css';
 
 export default function ProgramCard() {
   return (
-    <div>
+    <div className="carouselContainer">
       <div className="card mb-3 programCards">
         <img src={breakLine} className="card-img-top" alt="Break Line Tech" />
         <div className="card-body">
@@ -21,6 +22,30 @@ export default function ProgramCard() {
               <a
                 className="buttonStyle"
                 href="https://breakline.org/"
+                target="_blank"
+              >
+                Learn More
+              </a>
+            </button>
+          </p>
+        </div>
+      </div>
+      <div className="card mb-3 programCards">
+        <img src={shift} className="card-img-top" alt="Shift.org" />
+        <div className="card-body">
+          <h5 className="card-title">Shift.org</h5>
+          <p className="card-text">
+            From active duty servicemembers in the midst of their military
+            career to veterans who have made the transition to civilian life,
+            Shift’s community platform allows its members to engage in career
+            development services that keep them competitive in the current and
+            future economy.
+          </p>
+          <p className="card-text">
+            <button type="button" className="btn btn-primary">
+              <a
+                className="buttonStyle"
+                href="https://www.shift.org/"
                 target="_blank"
               >
                 Learn More
